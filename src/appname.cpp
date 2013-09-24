5a1f600c8079f618ba09f28281be986f4b4214fb
Jeff Pohlmeyer <yetanothergeek@gmail.com>
2013-09-23 22:12:58 -0500

    Updated copyright year

--- src/appname.cpp
+++ src/appname.cpp
@@ -1,6 +1,6 @@
 /*
   FXiTe - The Free eXtensIble Text Editor
-  Copyright (c) 2009-2012 Jeffrey Pohlmeyer <yetanothergeek@gmail.com>
+  Copyright (c) 2009-2013 Jeffrey Pohlmeyer <yetanothergeek@gmail.com>
 
   This program is free software; you can redistribute it and/or modify it
   under the terms of the GNU General Public License version 3 as
@@ -28,7 +28,7 @@
 
 static const char *Scintilla_Copyright =" \
 The Scintilla editing control and the PDF/HTML export code are\n\
-copyright 1998-2012 by Neil Hodgson <neilh@scintilla.org>\n\
+copyright 1998-2013 by Neil Hodgson <neilh@scintilla.org>\n\
 and are released under the following license:\n\
 \n\
 All Rights Reserved\n\
@@ -111,7 +111,7 @@ For more details, visit http://www.fox-toolkit.org\
 
 #define App_About "\
 Free eXtensIble Text Editor (FXiTe) %s %s\n\n\
-Copyright (c) 2009-2012 Jeffrey Pohlmeyer\n\
+Copyright (c) 2009-2013 Jeffrey Pohlmeyer\n\
 <%s>\n\n\
 %s\
 GNU GENERAL PUBLIC LICENSE Version 3\n\n\
