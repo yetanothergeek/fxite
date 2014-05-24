e501b8664068581f0bc14f00474fc9c03c72774f
Jeff Pohlmeyer <yetanothergeek@gmail.com>
2014-05-24 04:20:03 -0500

    Cast printf argument from lua_Integer to long (quiets compiler warning)

--- src/luafx.cpp
+++ src/luafx.cpp
@@ -173,7 +173,7 @@ static int input(lua_State*L)
   switch (toupper(type[0])) {
     case 'I' : {
       if (!lua_isnil(L,3)) {
-        txt.format(sizeof(lua_Integer)>4?"%ld":"%d", luaL_checkinteger(L,3));
+        txt.format("%ld", (long)luaL_checkinteger(L,3));
       }
       opt=INPUTDIALOG_INTEGER;
       break;
