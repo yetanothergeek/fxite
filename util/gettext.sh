e7de8d465a79054098dd999a3b734093e9ecbc07
Jeff Pohlmeyer <yetanothergeek@gmail.com>
2014-06-04 15:12:25 -0500

    Updated gettext files

--- util/gettext.sh
+++ util/gettext.sh
@@ -4,20 +4,19 @@
 
 rm -rf po m4 ABOUT-NLS config.rpath *~
 
-sed -i 's# *po/Makefile.in *# #g' configure.in
-
-
-echo 'SUBDIRS=src' > Makefile.am
-
 mv ChangeLog ChangeLog.TMP
 
 gettextize .
+
+sed -i 's/ po po / po /' Makefile.am
+sed -i 's/^EXTRA_DIST =.*/EXTRA_DIST = \\/' Makefile.am
+sed -i 's/AC_OUTPUT(.*/AC_OUTPUT( /' configure.ac
 
 cp po/Makevars.template po/Makevars
 
 rm -f *~
 
-grep -l '"intl\.h"' src/*.{c,cpp,h} | sort > po/POTFILES.in
+grep -l '"intl\.h"' {jef,mnumgr,src}/*.{c,cpp,h} | sort > po/POTFILES.in
 
 sed \
   's/^COPYRIGHT_HOLDER.*$/COPYRIGHT_HOLDER = Jeffrey Pohlmeyer <yetanothergeek@gmail.com>/' \
@@ -25,3 +24,4 @@ sed \
 
 
 mv ChangeLog.TMP ChangeLog
+
